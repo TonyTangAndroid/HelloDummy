0ae892518628e3ebdd68a6f7d1212e5a63208354
TonyTandAndroid <tonytangandroid@gmail.com>
2019-04-07 16:56:41 -0700

    Add a dummy library demo

---
+++ app/src/main/java/com/tony/tang/dummy/lib/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tony.tang.dummy.lib;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.tony.tang.dummy.lib.demo.DummyUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv_message = findViewById(R.id.tv_message);
+        tv_message.setText(DummyUtil.getDummyMessage());
+    }
+}
---
+++ lib/src/main/java/com/tony/tang/dummy/lib/demo/DummyUtil.java
@@ -0,0 +1,8 @@
+package com.tony.tang.dummy.lib.demo;
+
+public class DummyUtil {
+
+    public static String getDummyMessage() {
+        return "This is a dummy message from dummy library";
+    }
+}
