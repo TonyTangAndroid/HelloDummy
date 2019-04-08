ec9b134cf9946e6d9e5ff0323eb85e4cd1b57005
TonyTandAndroid <tonytangandroid@gmail.com>
2019-04-07 17:26:18 -0700

    Migrate to AndroidX

--- app/src/main/java/com/tony/tang/dummy/lib/MainActivity.java
+++ app/src/main/java/com/tony/tang/dummy/lib/MainActivity.java
@@ -1,7 +1,7 @@
 package com.tony.tang.dummy.lib;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.tony.tang.dummy.lib.demo.DummyUtil;
