dcb5dcd9bf5c48205c3fc44944313bd16b8889e4
TonyTandAndroid <tonytangandroid@gmail.com>
2019-04-07 17:39:06 -0700

    Add minimum code with androidx dependencies.

--- app/src/main/java/com/tony/tang/dummy/lib/MainActivity.java
+++ app/src/main/java/com/tony/tang/dummy/lib/MainActivity.java
@@ -1,18 +1,19 @@
 package com.tony.tang.dummy.lib;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.tony.tang.dummy.lib.demo.DummyUtil;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv_message = findViewById(R.id.tv_message);
-        tv_message.setText(DummyUtil.getDummyMessage());
+        tv_message.setText("Hello");
     }
 }
--- lib/src/main/java/com/tony/tang/dummy/lib/demo/DummyUtil.java
+++
@@ -1,8 +0,0 @@
-package com.tony.tang.dummy.lib.demo;
-
-public class DummyUtil {
-
-    public static String getDummyMessage() {
-        return "This is a dummy message from dummy library";
-    }
-}
